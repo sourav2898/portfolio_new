9413ee66c88dbfc0e5afc041e5b2a81b551c3aa0
sourav <souravkumardubey2898@gmail.com>
2021-12-15 17:40:03 -0300

    navbar responsive

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,30 +1,55 @@
-import React, {useEffect} from 'react'
+import React, {useEffect,useState} from 'react'
 import './navbar.scss'
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Hamburger from '@material-ui/icons/Menu';
+import Close from '@material-ui/icons/Close';
+import { Link } from '@material-ui/core';
 
 const Navbar = () => {
+    const [showNav, setShowNav] = useState(false);
     useEffect(() => {
         AOS.init({
             duration: 2000
         });
         AOS.refresh();
+        setShowNav(false)
       }, []);
+
+      const changeNav = () => {
+          setShowNav(!showNav);
+      }
 
     return (
         <nav className="navbar" id="navbar">
             <div className="max-width">
-                <div data-aos="fade-down" className="logo"><a href="#home">Portfo<span>lio</span></a></div>
+                <div data-aos="fade-down" className="logo"><Link href="#home">Portfo<span>lio</span></Link></div>
                 <ul className="menu">
-                    <li data-aos="fade-down"><a href="#home" className="menu-btn">Home</a></li>
-                    <li data-aos="fade-down"><a href="#about" className="menu-btn">About</a></li>
-                    <li data-aos="fade-down"><a href="#services" className="menu-btn">Services</a></li>
-                    <li data-aos="fade-down"><a href="#skills" className="menu-btn">Skills</a></li>
-                    <li data-aos="fade-down"><a href="#projects" className="menu-btn">Projects</a></li>
-                    <li data-aos="fade-down"><a href="#contact" className="menu-btn">Contact</a></li>
+                    <li data-aos="fade-down"><Link href="#home" className="menu-btn">Home</Link></li>
+                    <li data-aos="fade-down"><Link href="#about" className="menu-btn">About</Link></li>
+                    <li data-aos="fade-down"><Link href="#services" className="menu-btn">Services</Link></li>
+                    <li data-aos="fade-down"><Link href="#skills" className="menu-btn">Skills</Link></li>
+                    <li data-aos="fade-down"><Link href="#projects" className="menu-btn">Projects</Link></li>
+                    <li data-aos="fade-down"><Link href="#contact" className="menu-btn">Contact</Link></li>
                 </ul>
-                <div className="mobile"> 
-                    
+                <div className="mobile">
+                    {
+                        !showNav 
+                        ?
+                        <Hamburger data-aos="fade-left" onClick={changeNav}/>
+                        :
+                        <div data-aos="fade-left" className="nav_mob"> 
+                            <div className="mob_close"> <Close onClick={changeNav} /> </div>
+                            <ul className="menu_mob">
+                                <li data-aos="zoom-in"><Link href="#home" onClick={changeNav} className="menu-btn">Home</Link></li>
+                                <li data-aos="zoom-in"><Link onClick={changeNav} href="#about" className="menu-btn">About</Link></li>
+                                <li data-aos="zoom-in"><Link onClick={changeNav} href="#services" className="menu-btn">Services</Link></li>
+                                <li data-aos="zoom-in"><Link onClick={changeNav} href="#skills" className="menu-btn">Skills</Link></li>
+                                <li data-aos="zoom-in"><Link onClick={changeNav} href="#projects" className="menu-btn">Projects</Link></li>
+                                <li data-aos="zoom-in"><Link onClick={changeNav} href="#contact" className="menu-btn">Contact</Link></li>
+                            </ul>
+                        </div>  
+                    } 
                 </div>
             </div>
         </nav>
